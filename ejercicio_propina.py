a4b2be5c323677e6809d0b3baecf3b6eb06261b2
Marinayelena7 <lopez.negrete.elena@iescamas.es>
2024-09-30 10:10:32 +0200

    Add files via upload

---
+++ ejercicio_propina.py
@@ -0,0 +1,9 @@
+#Esto es un programa sobre propina
+print("Dime una cantidad")
+
+num1 = int(input())
+num2= float(0.15)
+#Realizamos la operación
+
+total = num1*num2
+print("L apropina del total seria", num1, "*", num2, "=", total, "€") 
