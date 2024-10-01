11c8ca49ff2c30c3e299e7bc888aba09bc93424a
Marinayelena7 <lopez.negrete.elena@iescamas.es>
2024-10-01 10:31:45 +0200

    Add files via upload

---
+++ Calculo_IMC.py
@@ -0,0 +1,9 @@
+#Esto es un programa para averiguar el IMC
+print("Dime tu peso kg")
+num1 = float(input())
+print("Dime tu altura m")
+num2= float(input())
+#Realizamos la operación
+
+total = num1/num2*num2 
+print("Su IMC", num1, "/", num2,"*",num2, "=", total, "IMC")
