687cc1f8812571dcfc83c53e2e6f0b3f77d96ece
Marinayelena7 <lopez.negrete.elena@iescamas.es>
2024-10-07 10:13:17 +0200

    actividad 4

---
+++ Conversion_de_unidades_de_conversión.py
@@ -0,0 +1,13 @@
+#Esto es un programa para convertir unidades 
+print("Dime cuantos metros tienes")
+num1 = int(input())
+#Realizamos la operación
+
+kilometros = num1/100
+metros = num1*100
+milímetros = num1*1000
+
+total1 = num1/100
+total2 = num1*100
+total3 = num1*1000
+print("La conversion es :",total1,"km", total2 ,"m", total3, "mm")
