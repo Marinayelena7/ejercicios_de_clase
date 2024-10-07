8eaed96d1f2d5ff2426fe3a8697578e413829844
Marinayelena7 <lopez.negrete.elena@iescamas.es>
2024-10-07 10:01:46 +0200

    Actividad 7

---
+++ calculo_media_notas.py
@@ -0,0 +1,12 @@
+#Esto es un programa para calcular la media de tu nota
+print("Dime la nota de tu primer exámen")
+
+num1 = float(input())
+print("Dime la nota de tu segundo exámen")
+num2 = float(input())
+print("Dime la nota de tu tercer exámen")
+num3 = float(input())
+#Realizamos la operación
+
+total = (num1+num2+num3)/3
+print("Tu media de este trimestre es",total)
