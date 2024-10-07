27118d87eae8235476776379713522092c31df52
Marinayelena7 <lopez.negrete.elena@iescamas.es>
2024-10-07 10:13:11 +0200

    Actividad 10

---
+++ Calculo_minutosysegundos_10.py
@@ -0,0 +1,10 @@
+#Esto es un programa para cambiar una cantidad de horas a minutos y segundos
+print("Dime la cantidad de horas que quieres cambiar")
+
+num1 = int(input())
+
+#Realizamos la operación
+
+total1 = num1*60
+total2 = num1*3600
+print("El resultado seria",total1, "min y", total2,"seg")
