ae2fa99957e1b78009d364bfc6026d771d377c2f
Marinayelena7 <lopez.negrete.elena@iescamas.es>
2024-10-07 09:31:17 +0200

    Add files via upload
    Actividad 8

---
+++ Calculo_de_edad8.py
@@ -0,0 +1,11 @@
+#Esto es un programa para calcular la edad actual 
+print("Dime el año en el que estamos")
+
+num1 = int(input())
+print("Dime tu año de nacimiento")
+num2 = int(input())
+
+#Realizamos la operación
+
+total = num1-num2
+print("Tienes", num1, "-", num2, "=", total, )
