36b7b142d7da7c931b587607b48b46e86e62f15f
Marinayelena7 <lopez.negrete.elena@iescamas.es>
2024-10-14 09:47:41 +0200

    Problemas condicionales ejercicio 2

---
+++ adivina_el_número.py
@@ -0,0 +1,8 @@
+#Esto es un prográma para adivinar el número
+NUMEROALEATORIO=3
+print("dime un número entre el 1 y el 10")
+número=int(input())
+if(número==NUMEROALEATORIO):
+    print("Lo has adivinado")
+else: print("No has acertado, sigue intentandolo")
+
